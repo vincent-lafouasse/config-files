19ca1719fdc28b7fc898294f560a01f7da5b8614
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2022-12-22 17:17:00 +0100

    format

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,43 +1,40 @@
 -- Install packer
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath('data') ..
+                         '/site/pack/packer/start/packer.nvim'
 local install_plugins = false
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  print('Installing packer...')
-  local packer_url = 'https://github.com/wbthomason/packer.nvim'
-  vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
-  print('Done.')
+    print('Installing packer...')
+    local packer_url = 'https://github.com/wbthomason/packer.nvim'
+    vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+    print('Done.')
 
-  vim.cmd('packadd packer.nvim')
-  install_plugins = true
+    vim.cmd('packadd packer.nvim')
+    install_plugins = true
 end
 
 -- plugins go in here
 require('packer').startup(function(use)
-  -- Package manager
-  use 'wbthomason/packer.nvim'
+    -- Package manager
+    use 'wbthomason/packer.nvim'
 
-	-- colorscheme + statusline
-  use 'morhetz/gruvbox'
-	use 'itchyny/lightline.vim'
+    -- colorscheme + statusline
+    use 'morhetz/gruvbox'
+    use 'itchyny/lightline.vim'
 
-	-- rainbow parenthesis
-	use 'luochen1990/rainbow'
+    -- rainbow parenthesis
+    use 'luochen1990/rainbow'
 
-	-- floating term
-	use 'voldikss/vim-floaterm'
+    -- floating term
+    use 'voldikss/vim-floaterm'
 
-	-- deal with bracket pairs etc
-	use {
-	"windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-}
+    -- deal with bracket pairs etc
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 
-  if install_plugins then
-    require('packer').sync()
-  end
+    if install_plugins then require('packer').sync() end
 end)
 
-if install_plugins then
-  return
-end
+if install_plugins then return end
