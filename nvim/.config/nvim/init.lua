296c1a5c97498eecf1b6ae94f30d14dac6d37986
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2022-12-25 18:11:03 +0100

    separate treesitter config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 require('plugins')
 require('settings')
 require('colorscheme-setup')
+require('treesitter-setup')
 require('mappings')
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -61,25 +61,7 @@ require('packer').startup(function(use)
     use {
         'nvim-treesitter/nvim-treesitter',
         requires = 'p00f/nvim-ts-rainbow',
-        run = ':TSUpdate',
-        config = function()
-            require'nvim-treesitter.configs'.setup {
-                ensure_installed = {
-                    "c", "lua", "rust", "cpp", "latex", "python"
-                },
-                auto_install = true,
-                highlight = {enable = true},
-                rainbow = {
-                    enable = true,
-                    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-                    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-                    max_file_lines = nil -- Do not enable for files with more than n lines, int
-                    -- colors = {}, -- table of hex strings
-                    -- termcolors = {} -- table of colour name strings
-                }
-            }
-        end
-
+        run = ':TSUpdate'
     }
 
     -- 42
