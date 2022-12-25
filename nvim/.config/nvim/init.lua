eaa36819ab44632b6d00afbf4f59a0929db76fe5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2022-12-25 17:17:03 +0100

    separate colorscheme setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,4 @@
 require('plugins')
 require('settings')
+require('colorscheme-setup')
 require('mappings')
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -20,46 +20,7 @@ require('packer').startup(function(use)
 
     -- colorscheme + statusline
     use 'itchyny/lightline.vim'
-    use {
-        "catppuccin/nvim",
-        as = "catppuccin",
-        config = function()
-            require("catppuccin").setup {
-                flavour = "mocha", -- latte, frappe, macchiato, mocha
-                term_colors = true,
-                transparent_background = false,
-                no_italic = false,
-                no_bold = false,
-                styles = {
-                    comments = {},
-                    conditionals = {},
-                    loops = {},
-                    functions = {},
-                    keywords = {},
-                    strings = {},
-                    variables = {},
-                    numbers = {},
-                    booleans = {},
-                    properties = {},
-                    types = {}
-                },
-                color_overrides = {mocha = {base = "#0A010B"}}, -- a dark purple
-                highlight_overrides = {
-                    mocha = function(C)
-                        return {
-                            TabLineSel = {bg = C.pink},
-                            NvimTreeNormal = {bg = C.none},
-                            CmpBorder = {fg = C.surface2},
-                            Pmenu = {bg = C.none},
-                            NormalFloat = {bg = C.none},
-                            TelescopeBorder = {link = "FloatBorder"}
-                        }
-                    end
-                }
-            }
-            vim.api.nvim_command "colorscheme catppuccin"
-        end
-    }
+    use {"catppuccin/nvim", as = "catppuccin"}
 
     -- File explorer
     use {
