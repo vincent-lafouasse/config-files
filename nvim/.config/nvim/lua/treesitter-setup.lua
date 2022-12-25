d505e2f8949cef077a15c3493a98513fe1015a0a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2022-12-25 20:06:47 +0100

    restructure

--- nvim/.config/nvim/lua/treesitter-setup.lua
+++ nvim/.config/nvim/lua/treesitter-setup.lua
@@ -11,3 +11,10 @@ require'nvim-treesitter.configs'.setup {
         -- termcolors = {} -- table of colour name strings
     }
 }
+
+-- Mappings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
