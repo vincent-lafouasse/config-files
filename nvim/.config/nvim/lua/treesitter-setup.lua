f77a21450e2589a01ab0d1aa12209682127f251a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-02-02 16:28:14 +0100

    add html to treesitter languages

--- nvim/.config/nvim/lua/treesitter-setup.lua
+++ nvim/.config/nvim/lua/treesitter-setup.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "rust", "cpp", "latex", "python"},
+    ensure_installed = {"c", "lua", "rust", "cpp", "latex", "python", "html"},
     auto_install = true,
     highlight = {enable = true},
     rainbow = {
