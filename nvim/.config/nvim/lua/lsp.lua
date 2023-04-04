8d48a5a34b24716b8a1478624e832c5e483297d8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-04-04 19:01:49 +0200

    stop complaining about `undefined global vim`

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,4 +1,13 @@
 require("mason").setup()
 require("mason-lspconfig").setup({ensure_installed = {"lua_ls"}})
 
-require("lspconfig").lua_ls.setup {}
+require("lspconfig").lua_ls.setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- stop complaining about `undefined global vim`
+                globals = {'vim'}
+            }
+        }
+    }
+}
