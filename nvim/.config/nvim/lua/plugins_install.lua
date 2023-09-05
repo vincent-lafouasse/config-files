1adf6d175a84716c08a302f847f7bfbe79e2ffc2
vincent lafouasse <poss@possmbp.home>
2023-09-05 17:36:32 +0200

    minor formatting

--- nvim/.config/nvim/lua/plugins_install.lua
+++ nvim/.config/nvim/lua/plugins_install.lua
@@ -1,25 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
+	"folke/which-key.nvim",
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	"folke/neodev.nvim",
 
-  {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  dependencies = { 'nvim-lua/plenary.nvim' }
-  },
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
 
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 })
