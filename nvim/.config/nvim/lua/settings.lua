dc5ff78d529420ea9b9c142c0fb59490f21cc84a
vincent lafouasse <poss@possmbp.home>
2023-09-06 00:12:36 +0200

    more basic settings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -5,3 +5,16 @@ vim.opt.smarttab = true
 vim.opt.expandtab = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.gdefault = true
+
+-- misc
+vim.opt.cmdheight = 0
+vim.opt.errorbells = false
+vim.opt.swapfile = false
