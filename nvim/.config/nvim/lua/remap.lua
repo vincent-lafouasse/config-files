104888273db74d10c589f63d3ac374d0208c0024
vincent lafouasse <poss@possmbp.home>
2023-09-05 21:25:28 +0200

    subst remaps

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -1,2 +1,10 @@
 vim.g.mapleader = " "
+
+-- netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
+
+-- subst
+vim.api.nvim_set_keymap('v', '<leader>ss', ":s/",
+                        {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<leader>ss', ":%s/",
+                        {noremap = true, silent = false})
