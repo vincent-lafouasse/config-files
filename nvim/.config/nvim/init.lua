5aeda3241823e569ac50610863cdf1dc1a8fb0d6
vincent lafouasse <poss@possmbp.home>
2023-09-05 17:35:20 +0200

    basic settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,4 @@
+require("settings")
 require("plugins_install")
 require("colorscheme")
 require("remap");
---
+++ nvim/.config/nvim/lua/settings.lua
@@ -0,0 +1,7 @@
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.opt.smarttab = true
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
