cdb976db642249eae3d36339c2d4cd9a81575346
vincent lafouasse <poss@possmbp.home>
2023-09-28 12:16:25 +0200

    navigation mappings

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -14,3 +14,17 @@ vim.api.nvim_set_keymap('n', '<leader>tt', ":FloatermToggle<cr>",
                         {noremap = true, silent = true})
 vim.api.nvim_set_keymap('t', '<leader>tt', "<C-\\><C-n>:FloatermToggle<CR>",
                         {noremap = true, silent = true})
+
+-- navigating panes
+	-- vim style navigation of panes 
+vim.api.nvim_set_keymap('n', '<leader>hh', ":wincmd h<CR>",
+                        {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<leader>jj', ":wincmd j<CR>",
+                        {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<leader>kk', ":wincmd k<CR>",
+                        {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<leader>ll', ":wincmd l<CR>",
+                        {noremap = true, silent = false})
+	-- go to previous
+vim.api.nvim_set_keymap('n', '<leader>pp', ":wincmd p<CR>",
+                        {noremap = true, silent = false})
