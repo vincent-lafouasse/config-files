109a3f4e23ad85908565787d26e74873af544f21
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-10 06:45:47 +0100

    expand tabs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,7 +9,7 @@ vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
 vim.opt.smarttab = true
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
